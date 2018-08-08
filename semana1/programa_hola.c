244d45c1ada24d56c5a45808071fd8507ce451be
Fran2dm <diazmj2018@licifug.ugto.mx>
2018-08-08 09:49:24 -0500

    cree un archivo :

---
+++ semana1/programa_hola.c
@@ -0,0 +1,15 @@
+/*
+Esto es un comentario. Me sirve para escribir información relevante del programa que estoy haciendo.Por ejemplo:
+Este programa imprime la típia frase "Hola mundo a la pantala hoy es 8/08/2018"
+*/
+/* Creado el 8 de agosto*/
+
+#include<stdio.h>
+       	int main()
+
+{
+
+	printf("Hola guapo\n");
+	printf("Este es el primer programa del curso\n");
+ }
+/*Termine de escribir el programa*/

dbb9aa79d7da5bb687caffaabf522ef24260a56f
Fran2dm <diazmj2018@licifug.ugto.mx>
2018-08-15 08:59:04 -0500

    Agregue el primer programa

---
+++ semana2/primer_programa.c
@@ -0,0 +1,13 @@
+/* Creado el 13 de agosto de 2018 por José Díaz*/
+
+#include<stdio.h>
+
+int main()
+{
+       int edad;
+ printf("Hola José\n");
+ printf("Este es el 1er programa del curso");
+ printf("21: \n);
+ scanf("%i",&edad);
+ printf("/nTu edad es: %i: \n",edad);
+return 0;v

69d0ec91b62d63630a9749ac7d36ef6ae09943f3
Fran2dm <diazmj2018@licifug.ugto.mx>
2018-09-03 09:56:46 -0500

    ejercicios no terminados :(

---
+++ semana5/ejemplo1.c
@@ -0,0 +1,30 @@
+/* Este es el primer ejemplo utilizando for*/
+
+#include<stdio.h>
+
+int main()
+{
+float c,k;
+float C,f,d;
+int n,i;
+d=(f-C)/n;
+printf("Hola\n");
+printf("Este programa imprime a la pantalla un listado n de temperaturas en Kelvin respecto a cierto rango\n");
+printf("Vamos a definir el rango de temperaturas en el que quieres trabajar\n");
+printf("NOTA: LA TEMPERATURA QUE VAS A INGRESAR A CONTINUACIÓN DEBE DE ESTAR DENTRO DEL RANGO DEFINIDO\n");
+scanf("%f %f",f,C);
+printf("Ingresa la temperatura en grados Celsius con la que quieras trabajar\n");
+scanf("%f",c);
+printf("
+
+
+
+printf("La temperatura que ingresaste es %f",c);
+
+for(i;i<n;i++){
+k=c+273.15;
+printf("%f %f\n",c,k);
+c=c+d;
+}
+return 0;
+}

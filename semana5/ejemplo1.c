9e5637b149a172b0567e24f5e60f7f34ca48fdd5
Fran2dm <diazmj2018@licifug.ugto.mx>
2018-09-05 10:05:00 -0500

    :(

--- semana5/ejemplo1.c
+++ semana5/ejemplo1.c
@@ -1,30 +1,33 @@
 /* Este es el primer ejemplo utilizando for*/
 
 #include<stdio.h>
-
 int main()
 {
-float c,k;
-float C,f,d;
-int n,i;
-d=(f-C)/n;
+float k;
+float C,f;
+int n,i,d,s;
+while(s<=1){
+
+
+
 printf("Hola\n");
 printf("Este programa imprime a la pantalla un listado n de temperaturas en Kelvin respecto a cierto rango\n");
-printf("Vamos a definir el rango de temperaturas en el que quieres trabajar\n");
-printf("NOTA: LA TEMPERATURA QUE VAS A INGRESAR A CONTINUACIÓN DEBE DE ESTAR DENTRO DEL RANGO DEFINIDO\n");
-scanf("%f %f",f,C);
-printf("Ingresa la temperatura en grados Celsius con la que quieras trabajar\n");
-scanf("%f",c);
-printf("
+printf("NOTA: ESTE PROGRAMA FUNCIONA UNICAMENTE CON CAMBIOS DE TEMPERATURA CON Ti>Tf\n");
+printf("Vamos a definir el rango de temperaturas en el que quieres trabajar, ingresa una temperatura inicial y después una final en grados centigrados \n");
+scanf("%f %f",&C,&f);
+printf("El intervalo que ingresaste esta entre %f %f\n",C,f);
+printf("Ingresa el número de renglones que quieres que te muestre\n");
+scanf("%i",&n);
 
+d=((f-C)/n);
+for(i=0;i<=n;i++){
+k=C+273.15;
+printf("%f %f %f\n",C,k,d);
+C=C+d;
+}
 
-
-printf("La temperatura que ingresaste es %f",c);
-
-for(i;i<n;i++){
-k=c+273.15;
-printf("%f %f\n",c,k);
-c=c+d;
+printf("¿Quieres realizar otra operación?, si presiona 1, no presiona 2\n");
+scanf("%i",&s);
 }
 return 0;
 }

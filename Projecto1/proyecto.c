6a65110baa050a0b5dbda81ad61057751e6d397a
Fran2dm <42146357+Fran2dm@users.noreply.github.com>
2018-09-30 17:17:48 -0500

    Update proyecto.c
    Termine el programa

--- Projecto1/proyecto.c
+++ Projecto1/proyecto.c
@@ -1,60 +1,42 @@
 /*Este programa fue creado por José Díaz Moya*/
+#include<stdio.h> 
+#include<math.h> 
 
-#include<stdio.h>
-#include<math.h>
+	int main(){ 
+	FILE*lec;  // Variable del archivo de lectura
+	FILE*write; //Variable del archivo de escritura 
 
-	int main (){
-		FILE *lec;
-		int N,c; //N es el tiempo de evolución que se va a graficar
-		double r,G=4.0*pow(3.14159265359,2),Ms,h; 
-	//r es el radio del planeta al sol, G es la constante de gravitación en años	Ms es la masa solar		
-		double vxo,vyo,vzo; //Vno=Velocidades iniciales
-		double vxi,vyi,vzi;	//Velocidades finales
-		double xo,yo,zo;	//Posiciones iniciales	
-		double xi,yi,zi;	//Posiciones finales
-		int i;			//Se utiliza para el for 
+		lec=fopen("Basededatos.txt","r"); //Abro mi archivo de lectura
+		double x[9],y[9],z[9],vx[9], vy[9], vz[9], r; // (x,y,z) son  las coordenadas del planeta (vx,vy,vz) son las velocidades del planeta, r es la distancia del planeta a la estrella.
+		float to[9], N[9], G=4*pow(3.14159265359,2), k, h; //to es el tiempo 0, N es el tiempo de evolución , G es la constante de gravitación en unidades astronómicas, h es l 
+		int i; //Se utiliza para el primer for
+		char nombre[20];  // Se utiliza la variable nombre para que vaya leyendo los datos de Basededatos y les ponga el nombre de la va
+
+		for(i=0; i<=8; i++){ 
 		
-			printf("Este programa calcula la órbita de los planetas del sistema solar \n");
-			printf("A partir de establecer un conjunto de posiciones y velocidades a un tiempo to y las actualiza a un tiempo posterior ti\n");
-			
-			lec=fopen("Neptuno1.txt","r"); //Se leen los datos del planeta tierra
-			fscanf(lec,"%lf %lf %lf %lf %lf %lf %lf %lf %i ",&xo,&yo,&zo,&vxo,&vyo,&vzo,&h,&Ms,&N);	
-			FILE *rs;
-			rs=fopen("Resultados.txt","w");	//Se prepara el programa para ingresar los resultados
-			
-			vxo*=365.242; //Se convierten las velocidades por día a velocidades por año que 365.242 es la cantidad de días por año
-			vyo*=365.242;		
-			vzo*=365.242;
-				fprintf(rs,"Día       x		     y		    z	          vx	         	 vy   	        vz       	 r\n");
-				c=0;
-				for(i=0;i<=N;i++){  //El for se utiliza para evaluar las operaciones que están dentro del mismo N veces de uno en uno 
-				
-			
-					xi=xo+(vxo*h);
-					yi=yo+(vyo*h);			//Las primeras 3 ecuaciones sirven para encontrar la posición después de un tiempo h
-					zi=zo+(vzo*h);  
-					r=sqrt(pow(xo,2)+pow(yo,2)+pow(zo,2));	//Se calcula el radio de la estrella al planeta
-					vxi=vxo-((h*G*Ms*xi)/pow(r,3));		
-					vyi=vyo-((h*G*Ms*yi)/pow(r,3));		//Se calculan las velocidades finales después de un tiempo h
-					vzi=vzo-((h*G*Ms*zi)/pow(r,3));
-					
-					xo=xi;
-					yo=yi;
-					zo=zi;		
- 	//Estas 6 ecuaciones se utilizan para volver a iniciar las ecuaciones anteriores pero ahora los datos obtenidos serán las posiciones y velocidades iniciales
-					vxo=vxi;
-					vyo=vyi;
-					vzo=vzi;
-					if(c%500==0){
-					fprintf(rs,"%i    	 %lf  	   %lf  	   %lf   	  %lf   	   %lf  	   %lf  	   %lf\n ",i,xi,yi,zi,vxi,vyi,vzi,r);	//Imprime los resultados en el archivo Resultados.txt			
-			
-			}
-		c++;			
-					
+			fscanf(lec,"%lf %lf %lf %lf %lf %lf %f %f %s %f", &x[i],&y[i],&z[i],&vx[i],&vy[i],&vz[i], &to[i], &N[i],&h, &nombre[i]));		
+			write=fopen(nombre,"w"); 	
+		for(k=to[i]; k<=N[i]+h ; k+=h){ 		 
 
-		}
+			x[i]=x[i]+vx[i]*h; 
+			y[i]=y[i]+vy[i]*h;  //Se utilizan para ir calculando las posiciones de cada planeta en cada paso de tiempo
+			z[i]=z[i]+vz[i]*h;
 
-		fclose(rs);
-		fclose(lec);
-	return 0;
-		}
+			
+			r=sqrt(pow(x[i],2)+pow(y[i],2)+pow(z[i],2)); // Calcula el radio del planeta a la estrella
+
+			vx[i]=vx[i]-h*((G*x[i])/pow(r,3));
+			vy[i]=vy[i]-h*((G*y[i])/pow(r,3)); //Se utilizan para calcular la velocidad del planeta en cada paso de tiempo
+			vz[i]=vz[i]-h*((G*z[i])/pow(r,3));	
+
+		
+			fprintf(write, "\n %f \t %lf \t %lf \t %lf \t %lf \t %lf \t %lf \n", k*365.2, x[i], y[i], z[i], vx[i], vy[i], vz[i]);
+			//Escribe en los archivos de los planetas las coordenadas (x,y,z) y la velocidad que lleva en sus componentes(vx,vy,vz)
+		} 
+		} 
+				fclose(write);  //Cierro el archivo de escritura
+				fclose(lec);    //Cierro el archivo de lectura
+		
+		
+				return 0; 
+					}

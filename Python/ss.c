8c8d02a50de0e3e173a1413103a67be449f9d433
Diaz Moya Jose Francisco <diazmj2018@dci25-159.ugto.mx>
2018-11-15 12:54:45 -0600

    Terminé ejercicio de movimiento oscilatorio

---
+++ Python/ss.c
@@ -0,0 +1,21 @@
+/* Programa del servicio social*/
+#include<stdio.h>
+
+	int main () {
+	
+
+		int i,j;	
+		FILE *fp
+		float matriz[N][N];
+		float x[N],y[N];
+		fp = fopen(".txt","r");
+		
+			fscanf(fp,"%f %f",&x,&y);
+
+		fclose (fp);
+		
+		for(i=0; i<=x; i++){
+			for(j=0; j<=x ; j++);{
+				
+				matriz[i][j];
+	
